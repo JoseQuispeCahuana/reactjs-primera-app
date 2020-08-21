5a6acfb047f9ff49190523ef91d2c61c0a36054a
JoseQuispeCahuana <lacarpetadelvergazo1@gmail.com>
2020-08-21 01:58:21 -0500

    Updates

---
+++ precache-manifest.5f3b71432c57ac76d04567562c4f18eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28a294186e469490c7064362f0e10093",
+    "url": "/reactjs-primera-app/index.html"
+  },
+  {
+    "revision": "5f81b78dd8cd81cda23e",
+    "url": "/reactjs-primera-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "48f1728eee67eecfc5f1",
+    "url": "/reactjs-primera-app/static/js/2.30267683.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactjs-primera-app/static/js/2.30267683.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f81b78dd8cd81cda23e",
+    "url": "/reactjs-primera-app/static/js/main.1c9f70d2.chunk.js"
+  },
+  {
+    "revision": "e2cc60f0e0536a3f985b",
+    "url": "/reactjs-primera-app/static/js/runtime-main.bca2978d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactjs-primera-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
